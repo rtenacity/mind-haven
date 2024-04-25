42313078b1819b87c69be5418bca94c4ce933b83
saksatti <saksatti@gmail.com>
2024-04-25 17:56:15 -0400

    timer stuff, still have to work on picker

--- frontend/screens/MeditationScreen.jsx
+++ frontend/screens/MeditationScreen.jsx
@@ -1,11 +1,47 @@
-import * as React from "react";
-import { View } from "react-native";
+import React, {useState} from "react";
+import { View, SafeAreaView, ScrollView, Image, Text } from "react-native";
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 import styles from "../styles";
+import { Button, Icon } from "@rneui/themed";
 
-export default function MeditationScreen({ props }) {
+export default function MeditationScreen({ route, navigation }) {
+    const [playing, setPlaying] = useState(true);
+    const { time } = route.params;
+    console.log(time);
     return (
-        <View>
-
-        </View>
+        <SafeAreaView style = {{backgroundColor: '#AEC5EB', flex:1}}>
+            <ScrollView>
+                <View style = {styles.realMeditationScreenContainer}>
+                    <Image source={require("../assets/landscape.jpg")} style={styles.meditationScreenImage} />
+                    <CountdownCircleTimer
+                    isPlaying={playing}
+                    duration={time}
+                    size={350}
+                    colors={['#8A7DDC']}
+                    trailColor={'#A7B7E8'}>
+                        {({ remainingTime }) =>
+                        <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+                            <Text style={styles.titleText}>{Math.floor(remainingTime/60)}{':'}{remainingTime % 60 < 10 ? '0' : ''}{Math.floor(remainingTime%60)}</Text>
+                            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 10}}>
+                                <Button buttonStyle={{borderRadius: 20, marginRight: 10}} color = '#8A7DDC' onPress={
+                                    () => {
+                                        setPlaying(false);
+                                    }
+                                }>
+                                    <Icon name="pause" type='ionicon' size={50}/></Button>
+                                <Button buttonStyle={{borderRadius: 20, marginRight: 10}} color = '#8A7DDC' onPress={
+                                    () => {
+                                        setPlaying(true);
+                                    }
+                                }>
+                                    <Icon name="controller-play" type='entypo' size={50}/></Button>
+                                <Button buttonStyle={{borderRadius: 20}} color = '#AD7DDC' onPress={() => {navigation.navigate('MeditationSetup')}}>
+                                    <Icon name="exit-outline" type='ionicon' size={50}/></Button>
+                            </View>
+                        </View>}
+                    </CountdownCircleTimer>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }

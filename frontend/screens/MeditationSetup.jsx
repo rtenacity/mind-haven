f2014ca82e1bfc0da38f2f699eb559594913797d
saksatti <saksatti@gmail.com>
2024-04-26 08:33:52 -0400

    make meditation scrollable

--- frontend/screens/MeditationSetup.jsx
+++ frontend/screens/MeditationSetup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, ScrollView } from 'react-native';
 import { Button, Icon } from '@rneui/themed';
 import styles from '../styles';
 import Header from '../component/Header';
@@ -43,63 +43,65 @@ export default function MeditationSetup({ navigation }) {
         
         <SafeAreaView style={[styles.dashboardContainer, { textAlign: 'center', alignItems: 'center' }]}>
             <Header navigation={navigation} />
-            <View style={{ paddingVertical: 10 }} />
+            <ScrollView>
+                <View style={[{ textAlign: 'center', alignItems: 'center'}]}>
+                    <CountdownCircleTimer
+                        key={time}
+                        isPlaying={playing}
+                        duration={time}
+                        size={300}
+                        colors={['#8A7DDC']}
+                        trailColor={'#A7B7E8'}
+                        onComplete={handleComplete}>
+                        {({ remainingTime }) => (
+                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={styles.titleText}>{Math.floor(remainingTime / 60)}:{remainingTime % 60 < 10 ? '0' : ''}{Math.floor(remainingTime % 60)}</Text>
+                                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 10 }}>
+                                    <Button buttonStyle={{ borderRadius: 20, marginRight: 10 }} color='#8A7DDC' onPress={() => setPlaying(false)}>
+                                        <Icon name="pause" type='ionicon' size={30} />
+                                    </Button>
+                                    <Button buttonStyle={{ borderRadius: 20, marginRight: 10 }} color='#8A7DDC' onPress={() => setPlaying(true)}>
+                                        <Icon name="controller-play" type='entypo' size={30} />
+                                    </Button>
+                                    <Button buttonStyle={{ borderRadius: 20 }} color='#AD7DDC' onPress={() => {
+                                        setPlaying(false);
+                                        setTime(0);
+                                    }}>
+                                        <Icon name="refresh" type='ionicon' size={30} />
+                                    </Button>
+                                </View>
+                            </View>
+                        )}
+                        </CountdownCircleTimer>
+                        <View style={{ paddingVertical: 10 }} />
 
-            <CountdownCircleTimer
-                key={time}
-                isPlaying={playing}
-                duration={time}
-                size={300}
-                colors={['#8A7DDC']}
-                trailColor={'#A7B7E8'}
-                onComplete={handleComplete}>
-                {({ remainingTime }) => (
-                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={styles.titleText}>{Math.floor(remainingTime / 60)}:{remainingTime % 60 < 10 ? '0' : ''}{Math.floor(remainingTime % 60)}</Text>
-                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 10 }}>
-                            <Button buttonStyle={{ borderRadius: 20, marginRight: 10 }} color='#8A7DDC' onPress={() => setPlaying(false)}>
-                                <Icon name="pause" type='ionicon' size={30} />
+                        <View style={styles.meditationButtonViewSetup}>
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(5)}}>
+                                <Text style={styles.meditationButtonText}>5 Min</Text>
                             </Button>
-                            <Button buttonStyle={{ borderRadius: 20, marginRight: 10 }} color='#8A7DDC' onPress={() => setPlaying(true)}>
-                                <Icon name="controller-play" type='entypo' size={30} />
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(10)}}>
+                                <Text style={styles.meditationButtonText}>10 Min</Text>
                             </Button>
-                            <Button buttonStyle={{ borderRadius: 20 }} color='#AD7DDC' onPress={() => {
-                                setPlaying(false);
-                                setTime(0);
-                            }}>
-                                <Icon name="refresh" type='ionicon' size={30} />
+                        </View>
+                        <View style={styles.meditationButtonViewSetup}>
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(15)}}>
+                                <Text style={styles.meditationButtonText}>15 Min</Text>
+                            </Button>
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(20)}}>
+                                <Text style={styles.meditationButtonText}>20 Min</Text>
+                            </Button>
+                        </View>
+                        <View style={styles.meditationButtonViewSetup}>
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(25)}}>
+                                <Text style={styles.meditationButtonText}>25 Min</Text>
+                            </Button>
+                            <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(30)}}>
+                                <Text style={styles.meditationButtonText}>30 Min</Text>
                             </Button>
                         </View>
                     </View>
-                )}
-            </CountdownCircleTimer>
-            <View style={{ paddingVertical: 10 }} />
-
-            <View style={styles.meditationButtonViewSetup}>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(0.1)}}>
-                    <Text style={styles.meditationButtonText}>5 Min</Text>
-                </Button>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(10)}}>
-                    <Text style={styles.meditationButtonText}>10 Min</Text>
-                </Button>
-            </View>
-            <View style={styles.meditationButtonViewSetup}>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(15)}}>
-                    <Text style={styles.meditationButtonText}>15 Min</Text>
-                </Button>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(20)}}>
-                    <Text style={styles.meditationButtonText}>20 Min</Text>
-                </Button>
-            </View>
-            <View style={styles.meditationButtonViewSetup}>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(25)}}>
-                    <Text style={styles.meditationButtonText}>25 Min</Text>
-                </Button>
-                <Button color="#8A7DDC" buttonStyle={styles.meditationButtonSetup} onPress={() => {handleTimeSet(30)}}>
-                    <Text style={styles.meditationButtonText}>30 Min</Text>
-                </Button>
-            </View>
-
+                    <View style = {{height: 150, width:20}}/>
+                </ScrollView>
             <NavigationBar nav={navigation} />
         </SafeAreaView>
     );
